14c1775032bda5903c805aa179a5d15a3127f92f
John Manos <john.m.manos@gmail.com>
2017-02-03 13:33:27 -0700

    Initial Commit

---
+++ InstagramActivityIndicator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramActivityIndicator
+//
+//  Created by John Manos on 2/3/17.
+//  Copyright © 2017 John Manos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
