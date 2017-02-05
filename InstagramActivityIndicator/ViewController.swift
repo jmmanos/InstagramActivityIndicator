33c7a3bb6d5d72bb6004e39e03e9c9e7876edac9
John Manos <john.m.manos@gmail.com>
2017-02-05 14:08:02 -0700

    Add line width animation. Add documentation. Add demo gifs.

--- InstagramActivityIndicator/ViewController.swift
+++ InstagramActivityIndicator/ViewController.swift
@@ -18,16 +18,5 @@ class ViewController: UIViewController {
             indicator.startAnimating()
         }
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
